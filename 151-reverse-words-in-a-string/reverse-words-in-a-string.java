29735e6c8d9a517f53c13d5a416a90f515297bb6
Akash120806 <akashind5399@gmail.com>
2026-05-18 03:16:42 +0530

    Time: 7 ms (64.67%) | Memory: 44.3 MB (68.98%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] arr= s.trim().split("\\s+");
+        StringBuilder st= new StringBuilder();
+        for(int i= arr.length-1;i>=0;i--){
+            st.append(arr[i]);
+            if(i!=0)st.append(" ");
+        }
+        return st.toString();
+    }
+}
