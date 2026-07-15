a811e21a58498cfab475e513db9d142a7b31ae11
Akash120806 <akashind5399@gmail.com>
2026-07-15 23:26:58 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (60.08%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,50 +1,44 @@
 class Solution {
-
     public int myAtoi(String s) {
 
         int i = 0;
         int n = s.length();
 
-        // Step 1: Skip leading spaces
         while (i < n && s.charAt(i) == ' ') {
             i++;
         }
 
-        // Step 2: Check sign
         int sign = 1;
-
         if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-
             if (s.charAt(i) == '-') {
                 sign = -1;
             }
-
             i++;
         }
 
-        // Step 3: Convert digits
-        int num = 0;
+        while (i < n && s.charAt(i) == '0') {
+            i++;
+        }
+
+        int result = 0;
 
         while (i < n && Character.isDigit(s.charAt(i))) {
 
             int digit = s.charAt(i) - '0';
 
-            // Step 4: Overflow check
-            if (num > Integer.MAX_VALUE / 10 ||
-               (num == Integer.MAX_VALUE / 10 && digit > 7)) {
+            if (result > Integer.MAX_VALUE / 10 ||
+               (result == Integer.MAX_VALUE / 10 && digit > 7)) {
 
-                if (sign == 1) {
+                if (sign == 1)
                     return Integer.MAX_VALUE;
-                } else {
+                else
                     return Integer.MIN_VALUE;
-                }
             }
 
-            num = num * 10 + digit;
-
+            result = result * 10 + digit;
             i++;
         }
 
-        return num * sign;
+        return result * sign;
     }
 }
